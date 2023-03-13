2fe7d9ac7bc3e49ad908c554517f2a8fe1e097c9
gsamue1 <122768087+gsamue1@users.noreply.github.com>
2023-03-13 00:06:51 -0400

    Adding Map

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//Mapping Access Token
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NhbXVlbC11b2Z0IiwiYSI6ImNsY3lieDA3MjJjNnAzcGs2NmxoMndpeGIifQ.PKKRKM7-HRYK7TuPgztVzg'; //default public map token from Mapbox account 
+
+//Activating Base Map
+const map = new mapboxgl.Map({
+    container: 'map', // div container ID for map
+    style: 'mapbox://styles/gsamuel-uoft/cle4l4pr5001t01ljheblcl08', // Link to mapbox style URL
+    center: [-79.390, 43.663], // starting position [longitude, latitude]
+    zoom: 11.29, // starting zoom
+});
